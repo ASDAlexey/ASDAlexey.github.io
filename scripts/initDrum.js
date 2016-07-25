83c4f4ebad918d673f78514c35b408b46d0e5036
Alexey Popov <asdalexey@yandex.ru>
2016-07-25 16:55:06 +0300

    init commit

---
+++ scripts/initDrum.js
@@ -0,0 +1,5 @@
+var drumOptions = { panelCount: 20, dail_w: 35, dail_h: 10, dail_stroke_width: 2 };
+$("#hours").drum(drumOptions);
+$("#minutes").drum(drumOptions);
+$("#seconds").drum(drumOptions);
+$("#hundredths").drum(drumOptions);
