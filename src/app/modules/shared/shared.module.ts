f226f495c28fa40965a22f431c8d17def2ee106a
alexey popov <alexey.popov@5thkind.com>
2021-06-29 17:32:57 +0300

    #1 rename currency pipe

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { NgForTrackByKeyDirective } from './directives/track-by-key.directive';
-import { CurrencySymbolPipe } from './pipes/CurrencySymbol.pipe';
+import { CurrencySymbolPipe } from './pipes/currency-symbol.pipe';
 
 const pipes = [CurrencySymbolPipe];
 const directives = [NgForTrackByKeyDirective];
