7b98678e8a9d9fa94ba6908fc279b98b43975594
Alexey Popov <asdalexey@yandex.ru>
2024-03-21 08:33:44 +0300

    fix: remove environment.development.ts from gitignore

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  environment: 'development',
+  API_URL: 'https://api.currencyfreaks.com/',
+  CURRENCY_FREAKS_API_KEY: 'b51102e92b9c4d10a5f3761598b2c9ba',
+};
